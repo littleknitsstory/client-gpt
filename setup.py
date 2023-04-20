a57676f593ba533708e959cb7a2fa4f02912171a
Яна Курзюкова <kurzukova.yana@yandex.ru>
2023-04-20 14:14:20 +0300

    black

--- setup.py
+++ setup.py
@@ -1,20 +1,19 @@
 from setuptools import setup
 
 setup(
-    name='client_GPT',
-    version='0.1',
-    packages=['client_api'],
+    name="client_GPT",
+    version="0.1",
+    packages=["client_api"],
     install_requires=[
-        'requests',
+        "requests",
     ],
-
     classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Intended Audience :: Developers',
-        'Topic :: Software Development :: Libraries',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Libraries",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
     ],
 )
